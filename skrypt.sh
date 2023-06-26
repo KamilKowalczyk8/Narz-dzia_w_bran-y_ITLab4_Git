62f6441a04883dffe713d1bd8393b775fdd2f09a
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2023-06-26 21:47:24 +0200

    Drugi commit

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-dzisiaj=$(date +"%Y-%m-%d")
-echo "Dzisiaj jest: $dzisiaj"
+for i in {1..100}; do echo "Nazwa pliku: log$i.txt" > log$i.txt;
+echo "Nazwa skryptu: skrypt.sh" >> log$i.txt;
+echo "Data: $(date)" >> log$i.txt;
+done
