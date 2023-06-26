265754ebcb667f8ee65949ed1849580ae3c18568
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2023-06-26 22:20:06 +0200

    Trzeci commit

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-for i in {1..100}; do echo "Nazwa pliku: log$i.txt" > log$i.txt;
-echo "Nazwa skryptu: skrypt.sh" >> log$i.txt;
-echo "Data: $(date)" >> log$i.txt;
+
+liczba_plikow=30; for i in $(seq $liczba_plikow); 
+do echo "Nazwa pliku: log$i.txt" > log$i.txt; 
+echo "Nazwa skryptu: skrypt.sh" >> log$i.txt; 
+echo "Data: $(date)" >> log$i.txt; 
 done
